bbf576ce0de8d42b702074e577b5e8d4d64eec99
FelipeEnne <felipeenne@gmail.com>
2020-06-04 22:27:22 -0300

    update the root index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-//import axios from 'axios';
+// import axios from 'axios';
 import list from '../initData';
 
 /* const URL = 'https://financialmodelingprep.com/api/v3/';
--- src/index.js
+++ src/index.js
@@ -9,7 +9,34 @@ import rootReducer from './reducers/index';
 
 const root = document.getElementById('root');
 
-const initialState = { market: '', status: '' };
+const initialState = {
+  market: [{
+    symbol: '^RUATR',
+    name: 'Russell 3000 Total Return',
+    price: 9805.96400000,
+    changesPercentage: -0.31000000,
+    change: -30.91100000,
+    dayLow: 9736.05000000,
+    dayHigh: 9856.27900000,
+    yearHigh: 9856.27900000,
+    yearLow: 9736.05000000,
+    marketCap: null,
+    priceAvg50: null,
+    priceAvg200: null,
+    volume: 0,
+    avgVolume: null,
+    exchange: 'INDEX',
+    open: 9830.31900000,
+    previousClose: 9836.87500000,
+    eps: null,
+    pe: null,
+    earningsAnnouncement: null,
+    sharesOutstanding: null,
+    timestamp: 1591316500,
+  }],
+
+  status: { loading: false, errors: [] },
+};
 
 const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
